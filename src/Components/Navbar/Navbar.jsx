3776d0f4ada663f6e977bc48d7396cbc97ec00ce
Evgeny-Laznevoy <laznevoy86@gmail.com>
2019-10-15 17:50:44 +0500

    using axios for HTTP requests, refactoring code with componentDidMount

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,6 +11,9 @@ const Navbar = () => {
             <NavLink to="/dialogs" activeClassName={s.active}>Message</NavLink>
         </div>
         <div className={s.item}>
+            <NavLink to="/users">Users</NavLink>
+        </div>
+        <div className={s.item}>
             <NavLink to="/news">News</NavLink>
         </div>
         <div className={s.item}>
